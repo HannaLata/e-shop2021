d09b858170d0f1d859a755fbe43e9b74ea14aab8
Hanna Lata <annalats32@gmail.com>
2021-05-20 17:59:43 +0300

    Create ItemDAO

--- src/main/java/com/hannalata/dao/ItemDAO.java
+++ src/main/java/com/hannalata/dao/ItemDAO.java
@@ -1,4 +1,150 @@
 package com.hannalata.dao;
 
+import com.hannalata.model.Item;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ItemDAO {
+
+    public static Item save(Item item) {
+        String sql = "INSERT INTO users (name, code, price, availability) " +
+                "VALUES (?, ?, ?, ?)";
+        String sequenceSQL = "SELECT currval(pg_get_serial_sequence('users','id'))";
+        int result = 0;
+        try (Connection connection = ConnectionToDB.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement(sql);
+             PreparedStatement sequenceStatement =
+                     connection.prepareStatement(sequenceSQL)) {
+            preparedStatement.setString(1, item.getName());
+            preparedStatement.setString(2, item.getCode());
+            preparedStatement.setInt(3, item.getPrice());
+            preparedStatement.setInt(4, item.getAvailability());
+
+            result = preparedStatement.executeUpdate();
+
+            if (result == 1) {
+                ResultSet resultSet = sequenceStatement.executeQuery();
+                while (resultSet.next()) {
+                    Integer id = resultSet.getInt(1);
+                    item.setId(id);
+                    break;
+
+                }
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return item;
+    }
+
+    public static List<Item> getAllAvailable() {
+        String sql = "SELECT * FROM users WHERE availability > 0";
+        List<Item> items = new ArrayList<>();
+        try (Connection connection = ConnectionToDB.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement(sql)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Item item = new Item(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("code"),
+                        resultSet.getInt("price"),
+                        resultSet.getInt("availability")
+                );
+                items.add(item);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    public static Item getById(Integer id) {
+        String sql = "SELECT * FROM users WHERE id = ?";
+        try (Connection connection = ConnectionToDB.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Item item = new Item(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("code"),
+                        resultSet.getInt("price"),
+                        resultSet.getInt("availability")
+                );
+                return item;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Item getByCode(String code) {
+        String sql = "SELECT * FROM users WHERE id = ?";
+        try (Connection connection = ConnectionToDB.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, code);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Item item = new Item(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("code"),
+                        resultSet.getInt("price"),
+                        resultSet.getInt("availability")
+                );
+                return item;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Item update(Item item) {
+        String sql =
+                "UPDATE users SET login = ?, password = ?, first_name = ?, last_name = ?, email = ?, phone = ? WHERE id = ?";
+
+        try (Connection connection = ConnectionToDB.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, item.getName());
+            preparedStatement.setString(2, item.getCode());
+            preparedStatement.setInt(3, item.getPrice());
+            preparedStatement.setInt(4, item.getAvailability());
+            preparedStatement.setInt(5, item.getId());
+            int result = preparedStatement.executeUpdate();
+            if(result == 1) {
+                return item;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void delete(Integer id) {
+        String sql = "DELETE FROM users WHERE id = ?";
+        try (Connection connection = ConnectionToDB.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
